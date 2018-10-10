8c43de86ddf2297cc65d54b494dd467ba5d9a363
Vanessa Nimmo <vanessanimmo@live.com>
2018-10-11 10:31:30 +1100

    test written for plant class to track weeks

--- lib/funny_plant.rb
+++ lib/funny_plant.rb
@@ -1,5 +1,5 @@
 class FunnyPlant
-  
+
   # Calculate the fruits grown by one plant in a given week after planting
   def fruits_per_plant(week_grown_in)
     week_grown_in - 1
--- spec/funny_plant_spec.rb
+++ spec/funny_plant_spec.rb
@@ -4,10 +4,16 @@ require 'funny_plant'
 describe FunnyPlant do
   context "When testing the FunnyPlant class" do
     plant = FunnyPlant.new
-    
+
     it 'should return 1 fruit grown in a week when one plant is in its second growing week' do
       fruits = plant.fruits_per_plant(2)
       expect(fruits).to eq 1
+    end
+
+    it 'should return week 0 when week is called immediately after initialization' do
+      plant = FunnyPlant.new
+      week = plant.week
+      expect(week).to eq 0
     end
   end
 end
