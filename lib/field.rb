84126268bc9e898af63388469af0f47812a26935
Vanessa Nimmo <vanessanimmo@live.com>
2018-10-11 11:08:40 +1100

    restructured Field class and added test

--- lib/field.rb
+++ lib/field.rb
@@ -1,6 +1,20 @@
+require 'funny_plant'
+
 class Field
 
-  def weeks(people, fruit)
+  def initialize(harvest, seeds)
+    @plants = plant(seeds)
+  end
+
+  def plant(seeds)
+    if @plants
+
+    else
+      Array.new(seeds) {FunnyPlant.new}
+    end
+  end
+
+  def weeks
     5
   end
 end
--- spec/field_spec.rb
+++ spec/field_spec.rb
@@ -3,11 +3,17 @@ require 'field'
 
 describe Field do
   context "When testing the Field class " do
-  field = Field.new
 
     it 'should return 5 weeks when given 15 people and 1 starting fruit' do
-      weeks = field.weeks(15, 1)
+      field = Field.new(15, 1)
+      weeks = field.weeks
       expect(weeks).to eq 5
+    end
+
+    it 'should return 14 weeks when given 50 000 people and 1 starting fruit' do
+      field = Field.new(50000, 1)
+      weeks = field.weeks
+      expect(weeks).to eq 14
     end
 
   end
